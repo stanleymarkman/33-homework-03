b1a80a90013d34da85afc9b535f591643db5edcd
tarbaugh <66973128+tarbaugh@users.noreply.github.com>
2021-04-19 16:14:29 -0400

    Add Login Page
    +Rick Roll

--- musicrater_react/src/App.js
+++ musicrater_react/src/App.js
@@ -19,7 +19,7 @@ function Copyright() {
   return (
     <Typography variant="body2" color="textSecondary" align="center">
       {'Copyright © '}
-      <Link color="inherit" href="https://material-ui.com/">
+      <Link color="inherit" href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">
         MusicR8R
       </Link>{' '}
       {new Date().getFullYear()}
